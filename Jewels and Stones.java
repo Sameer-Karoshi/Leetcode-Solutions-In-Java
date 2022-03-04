d4c6b3b979f91c790f816710923199922a964302
Sameer  Karoshi <65184810+Sameer-Karoshi@users.noreply.github.com>
2022-03-04 12:55:19 +0530

    Day 26: Jewels and Stones

---
+++ Jewels and Stones.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+       int result = 0;
+        for(int i = 0;i < jewels.length();i++){
+            char ch1 = jewels.charAt(i);
+            for(int j = 0;j < stones.length();j++){
+                char ch2 = stones.charAt(j);
+                if(ch1 == ch2){
+                    result++;
+                }
+            }
+        }
+        
+      return result;
+    }
+}
