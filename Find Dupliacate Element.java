46e83d8508a8c0f5a61c0aeffb0a6d6d3c1885b7
Sameer  Karoshi <65184810+Sameer-Karoshi@users.noreply.github.com>
2022-03-13 09:36:39 +0530

    Day 35: Find Duplicate Element

---
+++ Find Dupliacate Element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+        for(int i = 0;i < nums.length-1;i++){
+            if(nums[i] == nums[i+1]){
+                result = nums[i];
+                break;
+            }
+        }
+        return result;
+    }
+}
