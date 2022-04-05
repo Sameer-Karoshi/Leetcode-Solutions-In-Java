a52564630c57497179523a33b1f0c0ed1c75455b
Sameer  Karoshi <65184810+Sameer-Karoshi@users.noreply.github.com>
2022-04-05 13:37:48 +0530

    Day 57: Ugly Number

---
+++ Ugly Number.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n == 0){
+            return false;
+        }
+        boolean result = false;
+        while(n != 1){
+            if(n%2 == 0){
+                n = n/2;
+            }
+            else if(n%3 == 0){
+                n = n/3;
+            }
+            else if(n%5 == 0){
+                n = n/5;
+            }
+            else{
+                break;
+            }
+        }
+        if(n == 1){
+            return !result;
+        }
+        else if(n > 5){
+            return false;
+        }
+        return result;
+    }
+}
