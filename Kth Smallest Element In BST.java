9620d7139d972c42b90c74a52a628e0fea969b33
Sameer  Karoshi <65184810+Sameer-Karoshi@users.noreply.github.com>
2022-04-05 13:38:09 +0530

    Day 57: Kth Smallest Element In BST

---
+++ Kth Smallest Element In BST.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> list = new ArrayList<Integer>();
+    public int kthSmallest(TreeNode root, int k) {
+       Inorder(root); 
+       Collections.sort(list);
+       return list.get(k-1);
+    }
+    public void Inorder(TreeNode node){
+        if(node == null){
+            return;
+        }
+        Inorder(node.left);
+        list.add(node.val);
+        Inorder(node.right);
+    }
+}
