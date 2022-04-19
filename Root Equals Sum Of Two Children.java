4407d58bd956be397d0c138b927c553d7d3b2b86
Sameer  Karoshi <65184810+Sameer-Karoshi@users.noreply.github.com>
2022-04-19 09:31:57 +0530

    Day 71: Root Equals Sum Of Two Children

---
+++ Root Equals Sum Of Two Children.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+       int rootval = root.val;
+       int childsum = root.left.val + root.right.val;
+       if(rootval == childsum){
+           return true;
+       }
+       
+       return false;
+    }
+}
