05329da7f1acccdddaf2a0d8829aa4ca6c780be2
Sameer  Karoshi <65184810+Sameer-Karoshi@users.noreply.github.com>
2022-05-28 10:51:31 +0530

    Day 105: Perfect Numbers

--- Perfect Number.java
+++ Perfect Number.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
+        //these 5 are only perfect numbers
         return num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336;
     }
 }
