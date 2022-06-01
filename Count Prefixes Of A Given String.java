0c1157d297e02e1062479c6ea46eb513d9709976
Sameer  Karoshi <65184810+Sameer-Karoshi@users.noreply.github.com>
2022-06-01 11:19:59 +0530

    Day 109: Count Prefixes Of A Given String

---
+++ Count Prefixes Of A Given String.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countPrefixes(String[] words, String s) {
+        int res = 0;
+        for(int i = 0;i < words.length;i++){
+            if(s.startsWith(words[i])){
+                res++;
+            }
+        }
+        return res;
+    }
+}
