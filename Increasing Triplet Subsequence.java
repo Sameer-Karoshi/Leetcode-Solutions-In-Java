32a0a79bebd723a3a4255a9444be897441aa5ac0
Sameer  Karoshi <65184810+Sameer-Karoshi@users.noreply.github.com>
2022-06-02 20:55:15 +0530

    Day 110: Increasing Triplet Subsequence

---
+++ Increasing Triplet Subsequence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        if(nums==null || nums.length<3)
+            return false;
+        int first=Integer.MAX_VALUE,second=Integer.MAX_VALUE;
+        
+        for(int i:nums){
+            if(i<=first)first=i;
+            else if(i<=second)second=i;
+            else return true;
+        }
+        return false;
+    }
+}
