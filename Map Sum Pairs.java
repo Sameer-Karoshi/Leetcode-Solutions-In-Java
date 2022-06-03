81afbb6612638e1abf6cb3eb3cfcca6d3f8f8c56
Sameer  Karoshi <65184810+Sameer-Karoshi@users.noreply.github.com>
2022-06-03 19:48:38 +0530

    Day 111: Map Sum Pairs

---
+++ Map Sum Pairs.java
@@ -0,0 +1,28 @@
+class MapSum {
+    HashMap<String,Integer> map;
+    public MapSum() {
+       map = new HashMap<>(); 
+    }
+    
+    public void insert(String key, int val) {
+        map.put(key,val);
+    }
+    
+    public int sum(String prefix) {
+        int sum = 0;
+        for (Map.Entry<String,Integer> entry : map.entrySet()){
+            String key = (String)entry.getKey();
+            if(key.startsWith(prefix)){
+                sum = sum + map.get(key);
+            }
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum obj = new MapSum();
+ * obj.insert(key,val);
+ * int param_2 = obj.sum(prefix);
+ */
