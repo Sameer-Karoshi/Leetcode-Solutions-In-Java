85faca067c03a894bda7830d188e1f3f7a1a0f5d
Sameer  Karoshi <65184810+Sameer-Karoshi@users.noreply.github.com>
2022-06-26 16:56:43 +0530

    Day 132: Maximum Points You Can Obtain From Cards

---
+++ Maximum Points You Can Obtain From Cards.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int sublen = cardPoints.length - k;
+        //System.out.println(sublen);
+        int totalsum = 0;
+        for(int i: cardPoints){
+            totalsum = totalsum + i;
+        }
+        if(k == cardPoints.length){
+            return totalsum;
+        }
+        int minsubsum = Integer.MAX_VALUE;
+        for(int i = 0;i <= k;i++){
+            int temp = 0;
+            for(int j = i;j < i + sublen;j++){
+                temp = temp + cardPoints[j];
+            }
+            System.out.println(temp);
+            if(temp < minsubsum){
+                minsubsum = temp;
+            }
+        }
+        return totalsum - minsubsum;
+    }
+}
