58c3d89e81b7fd179b485d24bb6d57cd3fc0d7ef
Sameer  Karoshi <65184810+Sameer-Karoshi@users.noreply.github.com>
2022-08-24 22:31:45 +0530

    Day 180: Sum Of Subarray Ranges

---
+++ Sum Of Subarray Ranges.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        long res = 0;
+        for(int i = 0;i < nums.length;i++){
+            int max = nums[i];
+            int min = nums[i];
+            for(int j = i;j < nums.length;j++){
+                if(nums[j] > max){
+                    max = nums[j];
+                }
+                if(nums[j] < min){
+                    min = nums[j];
+                }
+                res += max-min;
+            }
+        }
+        return res;
+    }
+}
