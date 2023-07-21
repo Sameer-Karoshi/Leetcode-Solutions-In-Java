02e2a6c878d44b174c6bd1bf6335e8b8438340d5
Sameer  Karoshi <65184810+Sameer-Karoshi@users.noreply.github.com>
2023-07-21 11:36:15 +0530

    Day 216: Next Permutation

---
+++ Next Permutation.java
@@ -0,0 +1,49 @@
+public class Solution {
+    public void nextPermutation(int[] A) {
+        int n = A.length; // size of the array.
+
+        // Step 1: Find the break point:
+        int ind = -1; // break point
+        for (int i = n - 2; i >= 0; i--) {
+            if (A[i] < A[i + 1]) {
+                // index i is the break point
+                ind = i;
+                break;
+            }
+        }
+
+        // If break point does not exist:
+        if (ind == -1) {
+            // reverse the whole array:
+            reverse(A, 0);
+            return;
+        }
+
+        // Step 2: Find the next greater element
+        //         and swap it with arr[ind]:
+
+        for (int i = n - 1; i > ind; i--) {
+            if (A[i] > A[ind]) {
+                swap(A, i, ind);
+                break;
+            }
+        }
+
+        // Step 3: reverse the right half:
+        reverse(A, ind + 1);
+    }
+    private void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    private void reverse(int[] nums,int start){
+        int i=start;
+        int j=nums.length-1;
+        while(i<j){
+            swap(nums,i,j);
+            i++;
+            j--;
+        }
+    }
+}
